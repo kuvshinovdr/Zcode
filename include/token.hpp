069353063a75c8374abe090a956e26a4ef51ca70
Dmitry R. Kuvshinov <135211050+kuvshinovdr@users.noreply.github.com>
2024-01-31 01:15:09 +0500

    Create token.hpp

---
+++ include/token.hpp
@@ -0,0 +1,13 @@
+/// @file  zcode/token.hpp
+/// @brief Token definition and auxiliary operations.
+#ifndef ZCODE_TOKEN_HPP_INCLUDED
+#define ZCODE_TOKEN_HPP_INCLUDED
+
+namespace zcode::lex
+{
+
+
+
+}
+
+#endif//ZCODE_TOKEN_HPP_INCLUDED
