08a51ffa95165fb5f03c4ba7f30773e72a2008c1
Dmitry R. Kuvshinov <135211050+kuvshinovdr@users.noreply.github.com>
2024-01-31 01:24:51 +0500

    Update token.hpp
    split_to_lines declaration added

--- include/token.hpp
+++ include/token.hpp
@@ -3,10 +3,25 @@
 #ifndef ZCODE_TOKEN_HPP_INCLUDED
 #define ZCODE_TOKEN_HPP_INCLUDED
 
+#include <string>
+#include <vector>
+
+#include <string_view>
+#include <span>
+
+
+/// @brief Zcode lexer namespace
 namespace zcode::lex
 {
 
+  using std::u8string;
+  using std::u8string_view;
 
+  /// @brief       Split an input string into \n-separated lines.
+  /// @param input An input string that must persist while the resulting vector is being used.
+  /// @return      a vector of string views each holding one line of the input
+  [[nodiscard]] auto split_to_lines(u8string const& input)
+    -> std::vector<u8string_view>;
 
 }
 
